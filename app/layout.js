64eb3c97bc907c51bb010ffeb95bdd9d17c64ced
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2024-01-28 11:23:05 +0100

    Initial PWA config

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,13 @@
 import "./globals.css";
 
 export const metadata = {
+    manifest: "/manifest.json",
     title: "lov",
     description: "ziomkitrucks",
+};
+
+export const viewport = {
+    themeColor: "#FFFFFF",
 };
 
 export default function RootLayout({ children }) {
