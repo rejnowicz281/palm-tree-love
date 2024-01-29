a933a4ec6a0c135c5389a6969bb6927f380f128e
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2024-01-29 20:55:42 +0100

    Give Floating Hearts a wobbly animation when going up

--- app/components/FloatingHearts/index.js
+++ app/components/FloatingHearts/index.js
@@ -6,8 +6,20 @@ export default function FloatingHearts() {
         const randomSize = Math.random() * 50 + 50; // between 50 and 100
         const left = Math.random() * 100 + "%";
         const top = Math.random() * 100 + "%";
+        const randomWobbleLength = Math.floor(Math.random() * 10 + 5) / 10 + "s"; // between 0.5 and 1.5
 
-        return <Heart width={randomSize} height={randomSize} style={{ position: "absolute", top, left }} />;
+        return (
+            <Heart
+                width={randomSize}
+                height={randomSize}
+                style={{
+                    position: "absolute",
+                    top,
+                    left,
+                    animation: `${css.wobbly} ${randomWobbleLength} ease-in-out infinite`,
+                }}
+            />
+        );
     }
 
     return (
